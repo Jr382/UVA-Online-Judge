bc46977242554669abe7818aafc89f504b3f8db8
Jose <jose.perez-le@outlook.com>
2022-12-16 01:57:16 -0500

    Decorators implementation for 10131 B)

--- 10131.py
+++ 10131.py
@@ -1,6 +1,5 @@
 import sys
 sys.setrecursionlimit(100000)
-memory = {}
 
 
 def main():
@@ -11,21 +10,29 @@ def main():
         counter += 1
         line = sys.stdin.readline().strip()
     elephants.sort(key=lambda x: (x[0], -x[1]))
-    largest = dp(elephants, (float("-inf"), float("inf")))
-    print(str(len(largest))+"\n"+"\n".join(largest).strip())
+    response = dp(elephants)
+    print("{}\n{}".format(len(response), "\n".join(response).strip()))
 
 
+def memoization(function):
+    memory = {}
+
+    def wrapper(remaining, last=(float("-inf"), float("inf"))):
+        if last not in memory:
+            memory[last] = function(remaining, last)
+        return memory[last]
+
+    return wrapper
+
+
+@memoization
 def dp(remaining, last):
-    global memory
-    if last not in memory:
-        taken, not_taken = [], []
-        if len(remaining) > 0:
-            not_taken = dp(remaining[1:], last)
-            if compare(last, remaining[0]):
-                taken = [remaining[0][2]] + dp(remaining[1:], tuple(remaining[0]))
-        memory[last] = max(taken, not_taken, key=lambda x: len(x))
-
-    return memory[last]
+    ans = []
+    if len(remaining) > 0:
+        not_taken = dp(remaining[1:], last)
+        taken = [remaining[0][2]] + dp(remaining[1:], tuple(remaining[0])) if compare(last, remaining[0]) else []
+        ans = max(taken, not_taken, key=lambda x: len(x))
+    return ans
 
 
 def compare(elephant1, elephant2):
